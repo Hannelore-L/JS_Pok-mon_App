519aa69bbd5c5a381303f61d850477324fef97fb
Elyoneondra <hannelore.lmtt@gmail.com>
2020-04-08 18:04:39 +0200

    Making files and installing dependencies

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./styles.css";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Form from "./components/Form";
+import Results from "./components/Results";
+import Loading from "./components/Loading";
+import Monster from "./components/Monster";
+
+export default class App extends Component {
 }
-
-export default App;
